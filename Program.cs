308219878d875eebb100e4db1596187ec1981c58
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-03-01 14:37:25 +0530

    Review 4 Main

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Review_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ValidateEmailFormat.EmailValidate();
+        //ExtractPhoneNumbers.ExtractNumbers();
+        //CheckStrongPassword.CheckPassword();
+        //FindDates.FindingDates();
+        //ReplaceMultipleWhiteSpaces.ReplaceWhiteSpaces();
+        //CustomFileReadException.FileReadException
+        //CheckPalindromeUsingLamda.PalindromeUsingLamda();
+        //ReadAndWriteToFile.Data();
+        Employee.EmployeeMain();
+    
+    
+    }
+}
